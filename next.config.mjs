1f28ff43e8e2f75a34ebdd2b5ea7395eeaca319e
Michael Lo <michael.lo@buyandship.com.tw>
2024-02-17 04:37:30 +0800

    Add image domains to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexel.com"],
+  },
 };
 
 export default nextConfig
